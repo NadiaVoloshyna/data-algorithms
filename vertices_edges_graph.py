1008a33150fdfe423bd68be4e128b0d2e5dff685
NadiaVoloshyna <dnipronadiya@gmail.com>
2023-04-21 12:26:43 +0100

    modified graph implementation

--- vertices_edges_graph.py
+++ vertices_edges_graph.py
@@ -28,6 +28,7 @@ class Graph():
     def __init__(self):
         self.vertices = []
         self.edges = []
+        self.d = {}
 
     def add_vertex(self, x, y, name):
         self.vertices.append(Vertex(x, y, name))
@@ -41,8 +42,12 @@ class Graph():
     def get_edges(self):
         return self.edges
 
-    def get_vertex_dict(self):
-        return
+    def vertex_dict(self):
+        for el in self.vertices:
+            self.d.setdefault(el.get_name(), [])
+            for edge in self.edges:
+                if el.get_name() == edge.get_left_vertex().get_name():
+                    self.d[el.get_name()].append(edge.get_right_vertex().get_name())
 
     def __str__(self):
         s = ""
@@ -50,16 +55,37 @@ class Graph():
             s += f'x: {el.x} y: {el.y} name: {el.name} \n'
         for el in self.edges:
             s += f'left_edge: x: {el.left_vertex.x} y: {el.left_vertex.y} name: {el.left_vertex.name}   '
-            s += f'right_edge: x: {el.right_vertex.x} y: {el.right_vertex.y} name: {el.right_vertex.name}'
+            s += f'right_edge: x: {el.right_vertex.x} y: {el.right_vertex.y} name: {el.right_vertex.name} \n'
+        for (key, value) in self.d.items():
+            s += f'{key}: {value} \n'
         return s
 
 def demo():
-    a = Vertex(1,1,'A')
-    b = Vertex(3,3,'B')
+    # a = Vertex(1, 1, 'A')
+    # b = Vertex(3, 3, 'B')
+    # c = Vertex(5, 5, 'C')
+    # d = Vertex(7, 7, 'D')
+    # g = Graph()
+    # g.add_vertex(1, 1, 'A')
+    # g.add_vertex(3, 3, 'B')
+    # g.add_vertex(5, 5, 'C')
+    # g.add_vertex(7, 7, 'D')
+    # g.add_edge(a, b)
+    # g.add_edge(c, d)
+    # g.vertex_dict()
+    # print(g)
     g = Graph()
-    g.add_vertex(1,1,'A')
-    g.add_vertex(3,3,'B')
-    g.add_edge(a,b)
+    letter = 65
+    for x in range( 100, 500, 100 ):
+        g.add_vertex(x, x + 50, chr( letter ) )
+        letter += 1
+
+    v_list = g.get_vertices()
+    for v1 in v_list:
+        for v2 in v_list:
+            if v1.get_name() < v2.get_name():
+                g.add_edge(v1, v2)
+    g.vertex_dict()
     print(g)
 
 demo()
